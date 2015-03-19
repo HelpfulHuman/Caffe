24543800dfe527f7dff9e8bbbe7688ead237764f
Nick Glenn <nick@helpfulhuman.com>
2015-03-19 13:23:18 -0700

    Initial commit

---
+++ caffe.js
@@ -0,0 +1,25 @@
+/**
+ * Factory method for creating a new Caffe service.
+ *
+ * @param  {String} name
+ * @param  {Object} options
+ * @return {Service}
+ */
+exports.service = function (name, options) {
+
+}
+
+/**
+ * Factory method for creating a new Caffe client.
+ *
+ * @return {Client}
+ */
+exports.client = function () {
+
+}
+
+/**
+ * ALIASES
+ */
+exports.brew = exports.service
+exports.customer = exports.client
